ca5c974d9dec29cc80b293951c3d61fd65a023b3
cicley lee <cicley.lee@gmail.com>
2019-01-24 13:03:33 -0500

    Built project.

---
+++ C/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r, cir, area;
+            r = 0;
+
+            Console.WriteLine("Enter the radius:  ");
+            r = Convert.ToDouble(Console.ReadLine());
+
+            cir = 2 * Math.PI * r;
+            area = Math.PI * (r * r);
+
+            Console.WriteLine("The Circumference is: " + cir);
+            Console.WriteLine("The Area is: " + area);
+            Console.ReadLine();
+
+        }
+    }
+}
